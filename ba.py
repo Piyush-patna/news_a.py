3223a21f6470614d1a4deb8d5198e1e806357935
Piyush-patna <piyushkumar9415@gmail.com>
2025-06-03 11:46:33 +0530

    Update ba.py

--- ba.py
+++ ba.py
@@ -6,7 +6,7 @@ from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Embedding, LSTM, Dense
-from sklearn.model_selection import train_test_split
+
 
 df = pd.read_csv('Fake_Real_data.csv')
 df['Label'] = df['label'].map({'Fake': 0, 'Real': 1})
